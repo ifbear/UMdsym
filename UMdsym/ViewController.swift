0bd4ce548e6dba9a818748e969b7a7b8de0f415a
dexiong <dexiong@staff.sina.com>
2023-10-11 13:26:59 +0800

    Initial Commit

---
+++ UMdsym/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UMdsym
+//
+//  Created by dexiong on 2023/10/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
